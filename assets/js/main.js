ebb21568f4b312cb94cadfdf3da704274a213074
Haxu <nguyenngocminh190901@gmail.com>
2022-05-06 10:46:35 +0700

    them doc tu dong

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+Number.prototype.formatPrice = function(n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&.');
+};
+
+function getUrlImage(url) {
+    return './assets/images/item-image/' + url;
+}
+
+
+$(document).ready(function() {
+    
+    products.forEach(product => {
+        item = `
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="item">
+                    <a href="#" class="item-img-box">
+                        <img src="${getUrlImage(product.img)}" alt="" class="item-img">
+                    </a>
+                    <div class="item-text">
+                        <div class="item-name">
+                            ${product.name}
+                        </div>
+                        <div class="item-price">
+                            ${product.price.formatPrice()} VNĐ
+                        </div>
+                        <div class="item-control">
+                            <button class="btn" onclick="">
+                                Thêm vào giỏ hàng
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        if (product.type === 'headphone') {
+            $('#headphone .section-content .row').append($(item));
+        }
+    });
+
+})
---
+++ assets/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        "id": 1,
+        "name": "SENDY AUDIO PEACOCK",
+        "price": 34500000,
+        "img": "p1.jpg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+    {
+        "id": 2,
+        "name": "UNIQUE MELODY MEST MKII",
+        "price": 35590000,
+        "img": "p2.jpeg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+    {
+        "id": 3,
+        "name": "64 AUDIO U18S",
+        "price": 79500000,
+        "img": "p3.jpg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+    {
+        "id": 4,
+        "name": "SENNHEISER HD800S",
+        "price": 49999000,
+        "img": "p4.jpg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+    {
+        "id": 5,
+        "name": "HIFIMAN HE1000 V2",
+        "price": 69990000,
+        "img": "p5.jpg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+    {
+        "id": 6,
+        "name": "SONY MRD Z1R",
+        "price": 39990000,
+        "img": "p6.jpg",
+        'specification': [
+            "specification 1",
+            "specification 2",
+            "specification 3"
+        ],
+        "type": "headphone"
+    },
+]
